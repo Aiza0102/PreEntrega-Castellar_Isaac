a8d788f7ef0af7670bb8d1e95a9880cf71471b19
Aiza0102 <isaac.daniel102@hotmail.com>
2023-11-23 15:06:11 -0500

    SEGUNDA PREENGREGA PROYECTO, CONVERSOR DE MONEDA AUTONOMO

---
+++ js/moneda.js
@@ -0,0 +1,7 @@
+// moneda.js
+const tasasDeCambio = {
+    usd: 1,   // Dólares estadounidenses
+    eur: 0.85, // Euros
+    gbp: 0.75, // Libras esterlinas
+    // Agrega más tasas de cambio según sea necesario
+};
